79ca29de950028827df4666fd789bc028ebbd681
Abdullah Moubayed <abdullah.moubayed@outlook.com>
2021-11-15 11:56:11 +0100

    uppgift-5

---
+++ C-sharp/C-sharp/Program.cs
@@ -0,0 +1,19 @@
+﻿var random = new Random().Next(1, 101);
+Console.WriteLine(random);
+Console.WriteLine("Gissa talet!");
+var talet = 0;
+
+for (int i = talet; i != random;)
+{
+    talet = Int32.Parse(Console.ReadLine());
+    if (talet != random)
+    {
+        Console.WriteLine("Fel, gissa igen!");
+        continue;
+    }
+    else
+    {
+        Console.WriteLine("Rätt!");
+        break;
+    }
+}
