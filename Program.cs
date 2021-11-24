47d19149edc40de14e6e6620aabee016d1f86499
Abdullah Moubayed <abdullah.moubayed@outlook.com>
2021-11-24 11:57:17 +0100

    övning två

--- Program.cs
+++ Program.cs
@@ -1,15 +1,13 @@
-﻿Console.WriteLine("Ange bredd: ");
-int bredd = 10;
- 
-Console.WriteLine("Ange höjd: ");
-int höjd = 10;
- 
-var hash = "#";
-var zero = "0";
- 
-for (int i = 0; i < höjd; i++){
-    for (int j = 0; j < bredd; j++){
-        Console.WriteLine(hash + zero);
+﻿Console.WriteLine("inter some number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int col, row;
+
+for (row = 1; row <= num; row++)
+{
+    for (col = num; col >= row; col--)
+    {
+        Console.Write(" # ");
     }
-    Console.WriteLine(hash + zero);
+    
+    Console.WriteLine();
 }
