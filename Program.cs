e63e50ed419dab4ffc039881458bd1c751e2111d
Abdullah Moubayed <abdullah.moubayed@outlook.com>
2021-11-24 19:53:35 +0100

    uppgift 7

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿int månad = DateTime.Now.Month;
+
+static string getMonth(int month)
+{
+    int år = DateTime.Now.Year;
+    int dag = DateTime.Now.DayOfYear;
+    DateTime date = new DateTime(2021, month, 27);
+    return date.ToString("MMMM");
+}
+
+for (int i = 1; i <= månad; i++)
+{
+    Console.WriteLine(getMonth(i));
+}
