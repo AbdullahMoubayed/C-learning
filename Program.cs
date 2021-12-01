51c6f1aa7683ca3b088b6a2696d0b19b2e0f5af5
Abdullah Moubayed <abdullah.moubayed@outlook.com>
2021-12-01 10:49:39 +0100

    save

--- Program.cs
+++ Program.cs
@@ -1,14 +1,26 @@
 ﻿int månad = DateTime.Now.Month;
 
-static string getMonth(int month)
-{
+static String printMonday(int month){
     int år = DateTime.Now.Year;
-    int dag = DateTime.Now.DayOfYear;
-    DateTime date = new DateTime(2021, month, 27);
-    return date.ToString("MMMM");
+    int dag = DateTime.Now.Day;
+    var dayAtWeek = DateTime.Now.DayOfWeek;
+    var datum = "";
+
+    DateTime date = new DateTime(år, month,);
+
+    for (int i = 1; i <= date.Day; i++)
+    {
+        if (date.ToString("d") == "monday"){
+            DateTime date2 = new DateTime(2021, month, i);
+            datum = date.ToString("MMMM") + "-" + date.Day;
+        }        
+    }
+
+    return datum;
+
 }
 
-for (int i = 1; i <= månad; i++)
+for (var i = 2; i <= månad + 1; i++)
 {
-    Console.WriteLine(getMonth(i));
+    Console.WriteLine(printMonday(i));
 }
