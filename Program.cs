00fbfefadb1ee774ff36ebba8feed60ab9179ca0
Abdullah Moubayed <abdullah.moubayed@outlook.com>
2021-12-01 10:09:36 +0100

    finally

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿int år = DateTime.Now.Year;
+
+for (int i = år; i <= 2050; i++)
+{
+    var skolttÅr =  DateTime.IsLeapYear(i);
+
+    if (skolttÅr)
+    {
+        Console.WriteLine(i);
+    }
+}
+
+
