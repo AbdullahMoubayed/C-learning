cd61c3642dbb31a1cd9e69dba2c9bfe3545c1e2a
Abdullah Moubayed <abdullah.moubayed@outlook.com>
2021-12-06 08:49:01 +0100

    hej

--- Program.cs
+++ Program.cs
@@ -4,23 +4,23 @@ num = Convert.ToInt32(Console.ReadLine());
 
 for (row = 1; row <= num; row++)
 {
-    for (col = 1; col <= 1; col++) 
-    {
+  for (col = 1; col <= 1; col++) 
+  {
 
-        for (int i = 0; i <= num; i++)
-        {
-            Console.Write("0");
-            Console.Write("#");
-        }
-        Console.WriteLine();
-    }
-
-    for (int i = 0; i <= 5; i++)
+    for (int i = 0; i <= num; i++)
     {
-        Console.Write("#");
-        Console.Write("0");
+      Console.Write("0");
+      Console.Write("#");
     }
-    
     Console.WriteLine();
+  }
+
+  for (int i = 0; i <= 5; i++)
+  {
+    Console.Write("#");
+    Console.Write("0");
+  }
+  
+  Console.WriteLine();
 
 }
