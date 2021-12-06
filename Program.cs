f7191f9b749eeb87905a9e4f582e24951e089a85
Abdullah Moubayed <abdullah.moubayed@outlook.com>
2021-12-06 08:46:50 +0100

    finnally

--- Program.cs
+++ Program.cs
@@ -1,15 +1,15 @@
 ﻿Console.WriteLine("Ange bredd: ");
-int bredd = 10;
+int bredd = Int32.Parse(Console.ReadLine());
  
 Console.WriteLine("Ange höjd: ");
-int höjd = 10;
- 
+int höjd = Int32.Parse(Console.ReadLine());
+
 var hash = "#";
 var zero = "0";
  
 for (int i = 0; i < höjd; i++){
     for (int j = 0; j < bredd; j++){
-        Console.WriteLine(hash + zero);
+        Console.Write(zero + hash);
     }
  
     Console.WriteLine();
