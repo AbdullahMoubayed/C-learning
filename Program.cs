8963d9f0f6b368ce990139d67370f7157de0c5ef
Abdullah Moubayed <abdullah.moubayed@outlook.com>
2021-12-10 06:58:50 +0100

    start

--- Program.cs
+++ Program.cs
@@ -1,19 +1,49 @@
-﻿var random = new Random().Next(1, 101);
-Console.WriteLine(random);
-Console.WriteLine("Gissa talet!");
-var talet = 0;
+﻿String a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p;
 
-for (int i = talet; i != random;)
-{
-    talet = Int32.Parse(Console.ReadLine());
-    if (talet != random)
-    {
-        Console.WriteLine("Fel, gissa igen!");
-        continue;
-    }
-    else { 
-        Console.WriteLine("Rätt!");
-        break;
-    }
-}
-Console.WriteLine("Jajj, I can use vscode with c#");
+Console.WriteLine("Det kan finnas mer än två masters i ett Bluetooth nätverk.");
+var answer_a = Console.ReadLine();
+
+Console.WriteLine("Harald Blåtand uppfann Bluetooth tekniken.");
+var answer_b = Console.ReadLine();
+
+Console.WriteLine("Bluetooth nät av apparater kallas för mininät.");
+var answer_c = Console.ReadLine();
+
+Console.WriteLine("Bluetooth använder radiovågor.");
+var answer_d = Console.ReadLine();
+
+Console.WriteLine("Det kan finnas mer än två slavar i ett Bluetooth nätverk.");
+var answer_e = Console.ReadLine();
+
+Console.WriteLine("Bluetooth använder 2,4, 2,45 och 2,48 frekvenser.");
+var answer_f = Console.ReadLine();
+
+Console.WriteLine("1981 lanserades de första hemdatorerna. Bland tillverkarna  fanns sådana välkända företag som Apple.");
+var answer_g = Console.ReadLine();
+
+Console.WriteLine("SSd-disk är  Solid State disk innebär, en hårddisk uppbyggd av flash.minne. Helt utan rörliga delar.");
+var answer_h = Console.ReadLine();
+
+Console.WriteLine("DDR HyperX är en typ av DIMM minnen.");
+var answer_i = Console.ReadLine();
+
+Console.WriteLine("Standard bussen som finns på moderkortet är PCIe och PCI-Express.");
+var answer_j = Console.ReadLine();
+
+Console.WriteLine("Moderkortet  kallas ofta för CPU (Central Processing Unit).");
+var answer_k = Console.ReadLine();
+
+Console.WriteLine("Indataenhet = tangentbord, pekdon (mus), scanner.");
+var answer_l = Console.ReadLine();
+
+Console.WriteLine("I lådan monteras alla komponenter som tillhör själva datorn t.ex. tangentbord, pekdon (mus), scanner.");
+var answer_m = Console.ReadLine();
+
+Console.WriteLine("ENERGY STAR är ett internationellt frivilligt märkningsprogram för energieffektivitet");
+var answer_n = Console.ReadLine();
+
+Console.WriteLine("RoHS betyder Restriction of Hazardous Substances");
+var answer_o = Console.ReadLine();
+
+Console.WriteLine("Tandy var första elektroniska datorn tillverkades redan 1946.");
+var answer_p = Console.ReadLine();
